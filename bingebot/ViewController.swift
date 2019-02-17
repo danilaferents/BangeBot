81216b0fd78c5f449d17225f2914b7b6af005871
Danila Ferents <iambpev@gmail.com>
2019-02-17 21:08:03 +0300

    Initial Commit

---
+++ bingebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bingebot
+//
+//  Created by Мой Господин on 17/02/2019.
+//  Copyright © 2019 Danila Ferents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
